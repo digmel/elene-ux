199915ae5b13a5dace2416aae9f32a035db4370d
digmel <47932883+digmel@users.noreply.github.com>
2022-08-31 01:13:23 +0000

    Deploying to public from @ digmel/elene-ux@29e0b7b1d11d2f1e0f4e04c38135653f2e9e8eea 🚀

--- README.md
+++
@@ -1,7 +0,0 @@
-# eleneux.com
-
-https://twitter.com/EleneZed
-
-https://www.linkedin.com/in/elene-zed/
-
-https://www.facebook.com/elene.zed/
--- components/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./config/colors";
-export * from "./config/textStyles";
-export * from "./screen";
-export * from "./header";
-export * from "./footer";
-export * from "./heroSection";
-export * from "./card";
